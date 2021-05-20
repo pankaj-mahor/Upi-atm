8a14385be8091e242f949fd811b60c91fe4ba7af
DESKTOP\Pankaj Mahor <mahorpankaj12@gmail.com>
2021-05-20 15:52:04 +0530

    did atm one

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const btn = document.querySelectorAll('.btn');
 var str = '₹ ';
 const atmid = document.getElementById('atm-id');
-
+let circleAmount = document.getElementById('circle-amount');
 
 document.getElementById('form').addEventListener('submit',function(e){
-    e.preventDefault();
+    // e.preventDefault();
     // console.log(atmid.value)
+    if (circleAmount.value == 0) {
+      alert('value bhi dal bhai')
+    }
     validateDigit(atmid.value);
 })
 
 btn.forEach(element => {
-    let circleAmount = document.getElementById('circle-amount');
+    
     element.addEventListener('click' , function(){
       let updatedValue = (circleAmount.value = str.concat(element.value));
     })
@@ -23,7 +26,7 @@ function validateDigit(atmid) {
     console.log('yes');
     return true;
   }else{
-    console.log('no');
+    alert('Enter VAlid ATM id');
     return false;
   }
 }
